b2a0c8aacc7c57864fd00aa8b0547d2d79c4c720
supersam710 <spillers.sam@gmail.com>
2020-11-05 08:55:44 -0800

    add login

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const express = require('express')
 //Create a new instance of express
 const app = express()
 
-//let middleware = require('./utilities/middleware')
+let middleware = require('./utilities/middleware')
 
 const bodyParser = require("body-parser");
 //This allows parsing of the body of POST requests, that are encoded in JSON
 app.use(bodyParser.json())
 
 const { response, request } = require('express');
+
+app.use('/auth', require('./routes/login.js'))
 
 app.use('/auth', require('./routes/register.js'))
 
